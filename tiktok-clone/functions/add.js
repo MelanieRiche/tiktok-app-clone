a357ab230343343b415d0f161506111f5448019f
Melanie <contact.mriche@gmail.com>
2021-05-08 23:38:29 +0200

    Get data and listen to input with onChange

---
+++ tiktok-clone/functions/add.js
@@ -0,0 +1,35 @@
+const { createClient } = require("@astrajs/collections");
+
+const collection = 'tiktokposts'
+let id = faker.random.uuid()
+
+exports.handler = async function(event, context, callback) {
+    // create an Astra client
+    const astraClient = await createClient({
+        astraDatabaseId: process.env.ASTRA_DB_ID,
+        astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+        applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+    });
+
+    const users = astraClient
+        .namespace(process.env.ASTRA_DB_APPLICATION_KEYSPACE)
+        .collection(collection)
+       
+    try {
+        const user = await users.create(id, {
+
+        })
+        return {
+            statusCode: 200,
+        };
+    } catch (e) {
+        console.error(e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e)
+        }
+    }
+
+
+}
+
--- tiktok-clone/src/pages/Upload.js
+++ tiktok-clone/src/pages/Upload.js
@@ -1,12 +1,44 @@
+import React, { useState } from 'react'
+import faker from 'faker'
+
 
 const Upload = () => {
+  const username = 'rchmelanie'
+  const name = 'Mélanie Riché'
+  const avatar = 'https://lh3.googleusercontent.com/ogw/ADGmqu-yh_NmlP7YFVXn5QiN1LxvnSRcu_SisWvExSWzlDo=s83-c-mo'
+  let id = faker.random.uuid()
+  const today = new Date()
+  const timestamp = today.toISOString()
+  const [video, setVideo] = useState(null)
+  const [caption, setCaption] = useState(null)
+
+  console.log(caption)
+
+  const data = {
+    id: id,
+    name: name,
+    username: username,
+    avatar: avatar,
+    is_followed: false,
+    video: video,
+    caption: caption,
+    likes: 0,
+    comments: 0,
+    timestamp: timestamp,
+    button_visible: false,
+  }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className="upload-page">
         <br />
         <h1>Upload video</h1>
         <p>This video will be published to @username</p>
         <div className='container'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className='section'>
               <div className="image-upload"></div>
               <div className="form-section">
@@ -15,6 +47,7 @@ const Upload = () => {
                   <input
                     className='input'
                     name='caption'
+                    onChange={(e) => setCaption(e.target.value)}
                   />
                 </div>
                 <div className="break"></div>
@@ -23,6 +56,7 @@ const Upload = () => {
                   <input
                     className='input'
                     name='video'
+                    onChange={(e) => setVideo(e.target.value)}
                   />
                 </div>
               </div>
